1b6cadedb28471c8d73ec02760846fa84d46d2d9
JortvD <10116429+JortvD@users.noreply.github.com>
2019-09-21 00:24:39 +0200

    Update the help command

--- commands/help.js
+++ commands/help.js
@@ -4,12 +4,15 @@ module.exports = function(args) {
 	console.log("Usage: librimod <command>\n" +
 				"\n" +
 				"For <command> you can choose from:\n" +
-				"    create".padEnd(40, " ") + "Create your new librimod.\n" + 
-				"    help".padEnd(40, " ") + "Display this menu containing information about the command usage.\n" +
-				"    publish".padEnd(40, " ") + "Publish the unpublished version for the module to the librimod registry.\n" +
-				"    version".padEnd(40, " ") + "Upgrade the version of your librimod. Use \"librimod publish\" to push your changes to the registry.\n" +
-				"        --path, --minor, --major".padEnd(40, " ") + "Upgrade the version with a patch, minor or major change respectively.\n" +
-				"    login".padEnd(40, " ") + "Log into your account at the librimod registry. Accounts can be created from the librimod website.\n" +
-				"    logout".padEnd(40, " ") + "Log out of your current account."
+				"    create, c".padEnd(40, " ") + "Create your new librimod.\n" + 
+				"    help, h".padEnd(40, " ") + "Display this menu containing information about the command usage.\n" +
+				"    publish, p".padEnd(40, " ") + "Publish the unpublished version for the module to the librimod registry.\n" +
+				"    version, v".padEnd(40, " ") + "Upgrade the version of your librimod. Use \"librimod publish\" to push your changes to the registry.\n" +
+				"      --path, --minor, --major".padEnd(40, " ") + "Upgrade the version with a patch, minor or major change respectively.\n" +
+				"    test, t".padEnd(40, " ") + "Test your librimod using a predefined installation of Librify.\n" +
+				"    login, li <username> <password>".padEnd(40, " ") + "Log into your account at the librimod registry.\n" +
+				"    logout, lo".padEnd(40, " ") + "Log out of your current account.\n" +
+				"    register, r <username> <password>".padEnd(40, " ") + "Register a librimod account, needed for publishing librimods.\n" +
+				"    me".padEnd(40, " ") + "See whose account is currently logged in.\n"
 	);
 }

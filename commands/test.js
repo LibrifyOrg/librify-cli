89a4625e053db7dea55c1e5ba4585f6770acc498
JortvD <10116429+JortvD@users.noreply.github.com>
2019-09-21 00:23:28 +0200

    Add a test command

---
+++ commands/test.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const fs = require("fs");
+const rimraf = require("rimraf");
+const cp = require("child_process");
+const ncp = require("ncp").ncp;
+
+module.exports = function(args) {
+	let configPath = path.join(process.cwd(), "librimod.json");
+	let packagePath = path.join(process.cwd(), "package.json");
+	let config;
+
+	if(fs.existsSync(configPath)) {
+		config = require(configPath);
+	}
+	else {
+		try {
+			config = require(packagePath);
+		}
+		catch {
+			return console.error("ERR: Can't find the package.json");
+		}
+	}
+
+	if(config === undefined) {
+		return console.error("ERR: The librimod config wasn't found in librimod.json or package.json");
+	}
+
+	let testFolder = config.testFolder;
+
+	if(!testFolder) {
+		return console.error("ERR: No folder for the test installation has been provided in the config");
+	}
+
+	if(config.publish) {
+		console.log(`Executing the publish hook`);
+
+		process.stdout.write(cp.execSync(config.publish, {cwd: process.cwd()}).toString());
+	}
+
+	testFolder = path.join(process.cwd(), testFolder);
+	let pluginFolder = path.join(testFolder, "./plugins/", `./${config.name}/`);
+
+	if(!fs.existsSync(pluginFolder) || !fs.statSync(pluginFolder).isDirectory()) {
+		fs.mkdirSync(pluginFolder);
+	}
+
+	rimraf(pluginFolder, err => {
+		if(err) console.error(`ERR: There was an error while cleaning (${err.message})`);
+
+		if(config.dist) {
+			console.log(`Copying the dist folder to ${pluginFolder}`);
+	
+			ncp(path.join(process.cwd(), config.dist), pluginFolder, err => {
+				if(err) console.error(`ERR: There was an error while copying (${err.message})`);
+			});
+		}
+		else {
+			console.log(`Copying the project folder to ${pluginFolder}`);
+	
+			ncp(process.cwd(), pluginFolder, err => {
+				if(err) console.error(`ERR: There was an error while copying (${err.message})`);
+			});
+		}
+	});
+}

3a9608785342e0375b661006e3d1d3e66f2613b3
JortvD <10116429+JortvD@users.noreply.github.com>
2019-11-03 10:44:58 +0100

    Allow the build script to be configed and let it copy the config

--- defaults/build.js
+++ defaults/build.js
@@ -1,12 +1,36 @@
 const webpack = require("webpack");
 const util = require("util");
 const path = require("path");
-const packageConfig = require("./package.json");
+const fs = require("fs");
+
+let configPath = path.join(process.cwd(), "librimod.json");
+let packagePath = path.join(process.cwd(), "package.json");
+let config;
+
+if(fs.existsSync(configPath)) {
+	config = require(configPath);
+}
+else {
+	try {
+		config = require(packagePath);
+	}
+	catch {
+		return console.error("Can't find the package.json");
+	}
+}
+
+if(config === undefined) {
+	return console.error("The librimod config wasn't found in librimod.json or package.json");
+}
+
+const distFolder = path.resolve(process.cwd(), config.dist);
 const webpackConfig = {
-	entry: ["babel-polyfill", path.join(process.cwd(), packageConfig.index)].concat(packageConfig.entry || []),
+	entry: [path.join(process.cwd(), config.index)].concat(config.entry || []),
 	output: {
-		path: path.resolve(process.cwd(), packageConfig.dist),
-		filename: packageConfig.index
+		path: distFolder,
+		filename: config.index,
+		library: "",
+		libraryTarget: "commonjs2"
 	},
 	module: {
 		rules: [
@@ -29,11 +53,22 @@ const webpackConfig = {
 	target: "node"
 };
 
+if(!fs.existsSync(distFolder) || !fs.statSync(distFolder).isDirectory()) {
+	fs.mkdirSync(distFolder);
+}
+
+if(fs.existsSync(configPath)) {
+	fs.copyFileSync(configPath, path.join(distFolder, "librimod.json"));
+}
+else {
+	fs.copyFileSync(packagePath, path.join(distFolder, "librimod.json"));
+}
+
 const startTime = process.hrtime();
 
 util.promisify(webpack)(webpackConfig)
 .then(() => {
 	const endTime = process.hrtime(startTime);
 
-	console.log(`Build ${packageConfig.name} successfully in ${endTime[0]}s and ${endTime[1] / 1000000}ms`);
+	console.log(`Build successfully in ${endTime[0]}s and ${endTime[1] / 1000000}ms`);
 });

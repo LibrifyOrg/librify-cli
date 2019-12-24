9cfdfa5c59eafca16ae58167d68f92778efe4517
JortvD <10116429+JortvD@users.noreply.github.com>
2019-12-24 23:53:12 +0100

    Let the create command copy more files

--- commands/create.js
+++ commands/create.js
@@ -5,24 +5,31 @@ const fs = require("fs");
 const cp = require("child_process");
 const settings = require("../settings");
 
-module.exports = function(args) {
-	inquirer.prompt([
+module.exports = async function(args) {
+	const awnsers = await inquirer.prompt([
 		{type: "string", name: "name", message: "What will be the name of the librimod?", default: path.basename(process.cwd())},
 		{type: "string", name: "version", message: "What version will you start with?", default: "0.0.1"},
 		{type: "string", name: "author", message: "Who will be the author?", default: settings.username},
 		{type: "string", name: "index", message: "What will be the name of the index file?", default: "index.js"},
-	])
-	.then(awnsers => {
-		console.log("Generating build.js and package.json");
+	]);
+	
+	console.log("Generating build.js and package.json");
 
-		fs.copyFileSync(path.join(__dirname, "../defaults/build.js"), path.join(process.cwd(), "build.js"));
+	fs.copyFileSync(path.join(__dirname, "../defaults/build.js"), path.join(process.cwd(), "build.js"));
 
-		let packageData = fs.readFileSync(path.join(__dirname, "../defaults/package.json"));
+	if(!fs.existsSync(path.join(process.cwd(), awnsers.index))) {
+		fs.writeFileSync(path.join(process.cwd(), awnsers.index), "");
+	}
 
-		fs.writeFileSync(path.join(process.cwd(), "package.json"), mustache.render(packageData.toString(), awnsers));
+	if(!fs.existsSync(path.join(process.cwd(), ".gitignore"))) {
+		fs.copyFileSync(path.join(__dirname, "../defaults/.gitignore"), path.join(process.cwd(), ".gitignore"));
+	}
 
-		console.log("Installing all required modules");
+	let packageData = fs.readFileSync(path.join(__dirname, "../defaults/package.json"));
 
-		cp.execSync("npm install --save-dev");
-	});
+	fs.writeFileSync(path.join(process.cwd(), "package.json"), mustache.render(packageData.toString(), awnsers));
+
+	console.log("Installing all required modules");
+
+	cp.execSync("npm install --save-dev");
 }
